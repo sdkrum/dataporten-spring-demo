0fb1ea09ef523495f0a8ef7a0c9917d2614844ce
Søren Krum <sorenk@uninett.no>
2016-11-07 12:33:46 +0100

    Enhance code example

--- src/main/java/no/uninett/system/dataporten/demo/CustomLogoutSuccessHandler.java
+++ src/main/java/no/uninett/system/dataporten/demo/CustomLogoutSuccessHandler.java
@@ -5,14 +5,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 
 public class CustomLogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler {
 
-
-	@Override
-	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-	    throws IOException, ServletException {
-		super.onLogoutSuccess(request, response, authentication);
-	}
+  @Override
+  public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+    throws IOException, ServletException {
+    if (authentication != null) {
+      OAuth2Authentication oauth2auth = (OAuth2Authentication) authentication;
+      User user = (User) oauth2auth.getUserAuthentication().getDetails();
+      // do your stuff here...
+    }
+    super.onLogoutSuccess(request, response, authentication);
+  }
 }
